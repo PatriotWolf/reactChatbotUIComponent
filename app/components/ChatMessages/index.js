a42e18bbd4b9f485e106d4884bebfe7ef483a8b8
Mr.Worldwide <syah@hermo.my>
2018-08-03 19:57:39 +0800

    remove Text-To-Speech speak() API

--- app/components/ChatMessages/index.js
+++ app/components/ChatMessages/index.js
@@ -5,64 +5,66 @@
  */
 
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 
 import Avatar from 'images/robot01_90832.png';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
 function formatDate(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
-  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + " " + strTime;
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'pm' : 'am';
+    hours %= 12;
+    hours = hours || 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    const strTime = `${hours}:${minutes} ${ampm}`;
+    return `${date.getMonth() +
+        1}/${date.getDate()}/${date.getFullYear()} ${strTime}`;
 }
 
 function ChatMessages(props) {
     console.log(props.messages);
     return (
         <div className="messages-content container">
-
-            {
-                props.messages.map((element)=>{
-                    console.log(element)
-                    if (element.from === 'bot'){
-                        window.responsiveVoice.speak(element.text, "US English Male")
-                        return(
-                            <div className="message new" key={element.id}>
-                                <figure className="avatar">
-                                    <img src={Avatar}/>
-                                </figure>
-                                {element.text}
-                                <div className="timestamp">{formatDate(element.timestamp)}</div>
+            {props.messages.map(element => {
+                console.log(element);
+                if (element.from === 'bot') {
+                    return (
+                        <div className="message new" key={element.id}>
+                            <figure className="avatar">
+                                <img src={Avatar} alt="Bot's Avatar" />
+                            </figure>
+                            {element.text}
+                            <div className="timestamp">
+                                {formatDate(element.timestamp)}
                             </div>
-                        )
-                    }
-                    else {
-                        return (
-                            <div className="message message-personal new" key={element.id}>
-                                {element.text}
-                                <div className="timestamp">{formatDate(element.timestamp)}</div>
-                            </div>
-                        )
-                    }
-                })
-            }
+                        </div>
+                    );
+                }
+                return (
+                    <div
+                        className="message message-personal new"
+                        key={element.id}
+                    >
+                        {element.text}
+                        <div className="timestamp">
+                            {formatDate(element.timestamp)}
+                        </div>
+                    </div>
+                );
+            })}
             <div className="message loading new">
                 <figure className="avatar">
-                    <img src={Avatar} />
+                    <img src={Avatar} alt="Bot's Avatar" />
                 </figure>
-                <span></span>
+                <span />
             </div>
         </div>
     );
 }
 
-ChatMessages.propTypes = {};
+ChatMessages.propTypes = {
+    messages: PropTypes.array,
+};
 
 export default ChatMessages;
